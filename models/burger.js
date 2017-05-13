70b2111fad42c73e4a13965fb43c7261dc30f28b
Julie Burk <julieburk@gmail.com>
2017-05-13 14:01:57 -0700

    update

---
+++ models/burger.js
@@ -0,0 +1,11 @@
+
+
+module.exports = function(sequelize, DataTypes){
+  var Burgers = sequelize.define("burgers", {
+    burger_name: DataTypes.STRING,
+    devoured: DataTypes.BOOLEAN
+    });
+
+  return Burgers;
+}
+// module.exports = Burgers;
